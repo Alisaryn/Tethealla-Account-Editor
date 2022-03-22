3ce33466a48fa683da4768770031b32e218e484e
Michelle Powers <eternalx903@hotmail.com>
2022-03-22 01:57:51 -0400

    Add project files.

---
+++ Tethealla Account Editor/Form1.cs
@@ -0,0 +1,244 @@
+﻿/*
+    Tethealla account.dat Editor
+    Copyright 2022 Michelle Powers
+
+    Icon assets from the Tethealla project (https://pioneer2.net) were used in this project.
+    This project is reliant on Tethealla, but is not affiliated in any way with its developers.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tethealla_Account_Editor
+{
+    public partial class Form1 : Form
+    {
+        // Class global for user-chosen .dat file.
+        string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Exit context menu option.
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        // Save context menu option. Write contents from the datatable to our file.
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            int name_offset = 0x0, gcn_offset = 0x150, gm_offset = 0x154;
+            byte[] name_bytes, gcn_bytes;
+            string name_temp;
+            uint gcn_temp;
+            int num_to_pad;
+
+            using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Write, FileShare.None))
+            {
+                using (BinaryWriter reader = new BinaryWriter(fs))
+                {
+                    for (int i = 0; i < dataTable.RowCount; i++)
+                    {
+                        try
+                        {
+                            // Write account names.
+                            name_temp = dataTable.Rows[i].Cells[0].Value.ToString();
+                            name_temp = String.Concat(name_temp.Where(c => !Char.IsWhiteSpace(c)));
+                            name_temp = name_temp.Replace("\0", string.Empty);
+                            num_to_pad = 18 - name_temp.Length;
+                            name_temp += "\0";
+                            name_bytes = Encoding.Default.GetBytes(name_temp);
+                            for (int j = 1; j < num_to_pad; j++)
+                            {
+                                Array.Resize(ref name_bytes, name_bytes.Length + 1);
+                                name_bytes[name_bytes.Length - 1] = 0xCC;
+                            }
+                            fs.Seek(name_offset + (0x188 * i), 0);
+                            fs.Write(name_bytes, 0, 0x12);
+
+                            // Write Guild Card numbers.
+                            gcn_temp = UInt32.Parse(dataTable.Rows[i].Cells[1].Value.ToString());
+                            gcn_bytes = BitConverter.GetBytes(gcn_temp);
+                            fs.Seek(gcn_offset + (0x188 * i), 0);
+                            fs.Write(gcn_bytes, 0, 4);
+
+                            // Write global GM flags.
+                            fs.Seek(gm_offset + (0x188 * i), 0);
+                            if (dataTable.Rows[i].Cells[2].Value.ToString() == "Yes")
+                                fs.WriteByte(1);
+                            else fs.WriteByte(0);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+                    fs.Close();
+                }
+            }
+        }
+
+        // Parse/re-parse the account list.
+        private void parseAccounts()
+        {
+            dataTable.Rows.Clear(); // Clear all data when opening a new file.
+
+            using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    byte[] nameTemp = new byte[0x12];
+                    byte[] gcNum = new byte[0x04];
+                    bool isGlobalGM = false;
+                    int offset = 0;
+                    int eof = 0;
+
+                    while (eof != -1)
+                    {
+                        try
+                        {
+                            // Read account names.
+                            fs.Seek(offset, 0);
+                            fs.Read(nameTemp, 0, 0x12);
+
+                            // Read Guild Card numbers.
+                            offset += 0x150;
+                            fs.Seek(offset, 0);
+                            fs.Read(gcNum, 0, 0x04);
+
+                            // Read global GM flags.
+                            offset += 4;
+                            fs.Seek(offset, 0);
+                            isGlobalGM = Convert.ToBoolean(fs.ReadByte());
+
+                            // Need to strip name of all padded chars before adding to our text boxes!
+                            for (int i = 0; i < nameTemp.Length; i++)
+                            {
+                                if (nameTemp[i] == 0xCC)
+                                    nameTemp[i] = 0x0;
+                            }
+
+                            dataTable.Rows.Add(Encoding.Default.GetString(nameTemp), BitConverter.ToUInt32(gcNum, 0), isGlobalGM ? "Yes" : "No");
+                            offset += 0x34;
+                            fs.Seek(offset, 0);
+                            eof = fs.ReadByte(); // EOF check.
+                        }
+                        catch
+                        {
+                            fs.Close();
+                            return;
+                        }
+                    }
+
+                    fs.Close();
+                }
+            }
+        }
+
+        // Open context menu handler. Clear the table, then parse the accounts.
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = ".dat files (*.dat)|*.dat|All files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                fileName = dlg.FileName;
+                dataTable.Enabled = true;
+                mnuAddAccount.Enabled = true;
+                mnuSave.Enabled = true;
+                parseAccounts();               
+            }
+        }
+
+        private void dataTable_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            // Needed in order to fully save edits to our data table in case the the user doesn't leave a cell before saving!      
+            dataTable.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            // Convert to UInt32 for easy sorting.
+            try
+            {
+                if (dataTable.CurrentCell.ColumnIndex == 1)
+                    dataTable.CurrentCell.Value = UInt32.Parse(dataTable.CurrentCell.Value.ToString());
+            }
+            catch
+            {
+                dataTable.CurrentCell.Value = UInt32.MaxValue;
+                return;
+            }
+        }
+
+        private void dataTable_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Column1_KeyPress);
+
+            // Don't allow spaces in account names.
+            if (dataTable.CurrentCell.ColumnIndex == 0)
+            {
+                TextBox tb = e.Control as TextBox;
+                if (tb != null)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(Column1_KeyPress);
+                }
+            }
+
+            e.Control.KeyPress -= new KeyPressEventHandler(Column2_KeyPress);
+
+            // We also only want to allow number input for Guild Card numbers!
+            if (dataTable.CurrentCell.ColumnIndex == 1)
+            {
+                TextBox tb = e.Control as TextBox;
+                if (tb != null)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(Column2_KeyPress);
+                }
+            }
+        }
+        private void Column1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void Column2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Add account handler - start external "account_add.exe" process, then re-parse the account table if one is added.
+        private void addAccountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {       
+                Process process = new Process();
+                process.StartInfo.FileName = "account_add.exe";
+                process.Start();
+                process.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("account_add.exe was not found. Ensure that it is in the same directory as this program in order to use this option.", 
+                    "account_add.exe not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }           
+            parseAccounts();
+        }           
+    }
+}
---
+++ Tethealla Account Editor/Form1.designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Tethealla_Account_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAddAccount = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            this.accountNames = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gcNumbers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.globalGMs = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(321, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuAddAccount,
+            this.mnuSave,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(142, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuAddAccount
+            // 
+            this.mnuAddAccount.Enabled = false;
+            this.mnuAddAccount.Name = "mnuAddAccount";
+            this.mnuAddAccount.Size = new System.Drawing.Size(142, 22);
+            this.mnuAddAccount.Text = "Add account";
+            this.mnuAddAccount.Click += new System.EventHandler(this.addAccountToolStripMenuItem_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Enabled = false;
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(142, 22);
+            this.mnuSave.Text = "Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(142, 22);
+            this.mnuExit.Text = "Exit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // dataTable
+            // 
+            this.dataTable.AllowUserToAddRows = false;
+            this.dataTable.AllowUserToDeleteRows = false;
+            this.dataTable.AllowUserToResizeColumns = false;
+            this.dataTable.AllowUserToResizeRows = false;
+            this.dataTable.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.accountNames,
+            this.gcNumbers,
+            this.globalGMs});
+            this.dataTable.Enabled = false;
+            this.dataTable.Location = new System.Drawing.Point(10, 32);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.RowHeadersVisible = false;
+            this.dataTable.RowTemplate.Height = 25;
+            this.dataTable.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataTable.ShowCellToolTips = false;
+            this.dataTable.Size = new System.Drawing.Size(302, 173);
+            this.dataTable.TabIndex = 2;
+            this.dataTable.CurrentCellDirtyStateChanged += new System.EventHandler(this.dataTable_CurrentCellDirtyStateChanged);
+            this.dataTable.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataTable_EditingControlShowing);
+            // 
+            // accountNames
+            // 
+            this.accountNames.HeaderText = "Account name";
+            this.accountNames.MaxInputLength = 16;
+            this.accountNames.Name = "accountNames";
+            this.accountNames.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.accountNames.Width = 125;
+            // 
+            // gcNumbers
+            // 
+            this.gcNumbers.HeaderText = "GC#";
+            this.gcNumbers.MaxInputLength = 10;
+            this.gcNumbers.Name = "gcNumbers";
+            this.gcNumbers.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.gcNumbers.Width = 75;
+            // 
+            // globalGMs
+            // 
+            this.globalGMs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.globalGMs.HeaderText = "Global GM?";
+            this.globalGMs.Items.AddRange(new object[] {
+            "No",
+            "Yes"});
+            this.globalGMs.Name = "globalGMs";
+            this.globalGMs.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 214);
+            this.Controls.Add(this.dataTable);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tethealla account.dat Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.DataGridView dataTable;
+        private System.Windows.Forms.ToolStripMenuItem mnuAddAccount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn accountNames;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gcNumbers;
+        private System.Windows.Forms.DataGridViewComboBoxColumn globalGMs;
+    }
+}
+
